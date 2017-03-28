a23471403dba11a974012bacd608aaef5399924a
Nikita <laqwoter@gmail.com>
2017-03-28 05:17:01 +0300

    Add config comments

--- lib/cg1/config.rb
+++ lib/cg1/config.rb
@@ -1,10 +1,14 @@
 module Cg1
   # Configuration defaults
   @config = {
+              # default output file
               :output_path => "out.jpg",
+              # source of random words
               :words_source => '/usr/share/dict/words',
+              # flickr keys
               :flickr_api_key => "de5c5e7d36290a0d24dc007fa268d55e",
               :flickr_shared_secret => "2a74156825c64301",
+              # location of temporal image store folder
               :tmp_folder => './tmp'
             }
 
